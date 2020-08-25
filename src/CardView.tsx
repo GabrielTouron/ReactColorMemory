6b418cb1677b90553eaa6ec8c5b823a8d54a0024
Gabriel Touron <gtcode@pm.me>
2020-08-25 19:45:40 +0200

    card is reveal logic

--- src/CardView.tsx
+++ src/CardView.tsx
@@ -1,8 +1,38 @@
-import { Component } from "react";
+import React, { Component } from "react";
+import ICard from "./models/Card";
 
-class Cardview extends Component {
-    
+class Cardview extends Component<ICard> {
     // When a card is reveal
+
+    constructor(props: ICard) {
+        super(props);
+        this.onClick = this.onClick.bind(this);
+    }
+
+    onClick() {
+        if (!this.props.matched && !this.props.imageUp) {
+            this.props.onClick(this.props.id, this.props.image);
+        }
+    }
+
+    render() {
+        let path = './img/';
+        if (this.props.imageUp) {
+            path += `${this.props.image}.jpeg`
+        } else {
+            path += `back.jpeg`;
+        }
+
+        let className = 'card';
+        if (this.props.matched) {
+            className += ` matched`;
+        }
+
+        return (
+            <img className={className} src={require(`${path}`)} alt = '' onClick={this.onClick} />
+        );
+    };
+
 }
 
 export default Cardview;
