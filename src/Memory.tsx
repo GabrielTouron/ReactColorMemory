b7ba5ad409d9ccc80683e22df54585561eeb17e8
Gabriel Touron <gtcode@pm.me>
2020-08-25 19:23:45 +0200

    add memory game project structure

---
+++ src/Memory.tsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import './Memory.css';
+
+class Memory extends Component {
+
+    // Main class: memory game logic
+
+    render() {
+        return <div></div>
+    }
+
+}
+
+export default Memory;
