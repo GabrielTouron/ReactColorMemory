4de35eb7ea8adbc27376a89df6641f7944a54b6b
Gabriel Touron <gtcode@pm.me>
2020-08-25 20:39:29 +0200

    logic and css

--- src/Memory.tsx
+++ src/Memory.tsx
@@ -10,6 +10,8 @@ class Memory extends Component {
     cards: Cards;
     state: IState;
     timeout: any;
+    isLooser = 25;
+
 
     constructor(props: {}) {
         super(props);
@@ -97,6 +99,10 @@ class Memory extends Component {
                 }, 5000);
             }
         }
+        
+        if (this.state.round >= this.isLooser) {
+            this.boardCreation();
+        }
     }
 
     onPlayAgain() {
@@ -113,18 +119,28 @@ class Memory extends Component {
         if (this.state?.pairsFound === this.cards.NUM_IMAGES) {
             gameStat = <div className='memory-status'>
                 <div>GAGNÉ <span role="img" aria-label=''>🏅🏅🏅</span> !</div>
-                <div> En {this.state?.round - 1} tours !</div>
+                <div> En seulement {this.state?.round - 1} tours !</div>
+                <div><button className='btn' onClick={this.onPlayAgain}>Jouer encore ?</button></div>
+            </div>
+        } else if (this.state?.round >= this.isLooser) {
+            gameStat = <div className='memory-status'>
+                <div>PERDU <span role="img" aria-label=''>😞😞😞</span> !</div>
                 <div><button className='btn' onClick={this.onPlayAgain}>Jouer encore ?</button></div>
             </div>
         }
+
 
         return (
             <div className='memory'>
                 <header className='memory-header'>
                     <div className='memory-title'>Colors Memory en React</div>
+                    <p>Vous devez réussir en moins de 25 tours !</p>
                 </header>
                 <div>
                     {gameStat}
+                </div>
+                <div>
+
                 </div>
                 <div className='card-container'>
                     {cardViews}
