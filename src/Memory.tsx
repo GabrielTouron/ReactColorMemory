655e3a71efb20fa28ccd5b4c92b9d20f7a2a03ed
Gabriel Touron <gtcode@pm.me>
2020-08-25 19:49:34 +0200

    board creation logic

--- src/Memory.tsx
+++ src/Memory.tsx
@@ -2,17 +2,33 @@ import React, { Component } from "react";
 import Cards from "./Cards";
 
 import './Memory.css';
+import IState from "./models/State";
 
 class Memory extends Component {
     // Main class: memory game logic
 
     cards: Cards;
+    state: IState;
+
     constructor(props: {}) {
         super(props);
         this.cards = new Cards();
-        this.cards.cardGeneration();
     }
 
+    componentDidMount() {
+        this.boardCreation();
+    }
+
+    boardCreation() {
+        this.cards.cardGeneration();
+        this.setState({
+            round: 1,
+            pairsFound: 0,
+            nbrClicks: 0,
+            firstId: undefined,
+            secondId: undefined
+        })
+    }
 
     render() {
         return <div></div>
